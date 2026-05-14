5c2e1f13dadde9b9434d306aaf09c0363ed981e6
Dylan Shah <code@dylan-shah.com>
2026-05-13 19:54:14 -0700

    Init Day One

---
+++ 2025/Day1/main.cpp
@@ -0,0 +1,34 @@
+/**
+ * \file main.cpp
+ * \author Dylan Shah (code@dylan-shah.com)
+ * \brief My Day One Solution
+ * \ref https://adventofcode.com/2025/day/1 "Problem"
+ * \version 0.1
+ * \date 2026-05-13
+ *
+ * \copyright Copyright (c) 2026
+ *
+ * \parblock License:
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * \endparblock
+ *
+ */
+
+#include <iostream>
+
+int main(int argc, char const* argv[]) {
+    std::cout << "test";
+    return 0;
+}
